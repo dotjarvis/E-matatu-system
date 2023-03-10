eac66fbf6b38884a931f3b3e8ea5603253ee57d4
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-03-10 23:14:51 +0300

    11th commit
    Migrating my db to firebase from default sqlite

---
+++ matatu/firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyBbWQdwRKPq0724pf_qvTamVh9pA1Khnkg",
+  "authDomain": "ma3-project.firebaseapp.com",
+  "databaseURL": "https://ma3-project-default-rtdb.firebaseio.com",
+  "projectId": "ma3-project",
+  "storageBucket": "ma3-project.appspot.com",
+  "messagingSenderId": "84373379304",
+  "appId": "1:84373379304:web:db0eb449bda6fdf6fbd15d",
+  "measurementId": "G-KJLLNNX73Y"
+};
--- matatu/views.py
+++ matatu/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .firebase_config import firebaseConfig
+import pyrebase
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
 # Create your views here.
+auth = firebase.auth()
+
+
+def signin(request):
+    return render(request, 'matatu/signin.html')
